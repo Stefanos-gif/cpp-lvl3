2fc9e3d1be1189798c58dac73182cb1d56ebca3c
Stefanos-gif <82211587+Stefanos-gif@users.noreply.github.com>
2024-08-28 14:27:26 +0200

    Create linear search.cpp

---
+++ 5.Arrays and Algorithms/linear search.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int main(){
+
+    int arr[] = {69, 21, 420, 699, 1, 2, 3, 4, 5, 6, 7, 8};
+    for (int i = 0; i <12 ; i++){
+
+        if (arr[i] == 7){
+            std::cout << i + 1 << std::endl;
+        }
+
+
+
+    }
+    
+
+
+    return 0;
+}
